30f883e806d1030a35cc986287915e1d41a491f6
vyshaksm <122250107+vyshaksm@users.noreply.github.com>
2026-04-19 10:31:19 +0530

    molecule library manager logic change
    added a method to always add the newly found molecule to the op of the list.

--- Assets/_Scripts/MoleculeLibraryManager.cs
+++ Assets/_Scripts/MoleculeLibraryManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class MoleculeLibraryManager : MonoBehaviour
 {
@@ -18,24 +19,38 @@ public class MoleculeLibraryManager : MonoBehaviour
     // Rebuilds the scroll list from the molecule database.
     private void PopulateLibrary()
     {
-        foreach (Transform child in contentPanel) Destroy(child.gameObject);
+        foreach (Transform child in contentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+        activeUIItems.Clear();
 
         foreach (var molecule in db.allMolecules)
         {
             GameObject newItem = Instantiate(uiItemPrefab, contentPanel);
             MoleculeUIItem script = newItem.GetComponent<MoleculeUIItem>();
 
-            script.Setup(molecule.moleculeName, molecule.formula);
-            activeUIItems.Add(molecule.formula, script);
+            if (script != null)
+            {
+                script.Setup(molecule.moleculeName, molecule.formula);
+                activeUIItems.Add(molecule.formula, script);
+            }
         }
     }
 
-    // Marks a discovered molecule entry as unlocked in the UI.
+    // Marks a discovered molecule entry as unlocked in the UI and moves it to the top.
     public void NotifyDiscovery(string formula)
     {
         if (activeUIItems.ContainsKey(formula))
         {
             activeUIItems[formula].MarkAsDiscovered();
+            activeUIItems[formula].transform.SetAsFirstSibling();
+            LayoutRebuilder.ForceRebuildLayoutImmediate(contentPanel.GetComponent<RectTransform>());
+            ScrollRect scrollRect = GetComponentInParent<ScrollRect>();
+            if (scrollRect != null)
+            {
+                scrollRect.verticalNormalizedPosition = 1f;
+            }
         }
     }
 }
